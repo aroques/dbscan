c6ca5239ccd60d271f7b084bf0678cc0a7de9f80
Alec Roques <alec.roques@gmail.com>
2018-05-01 15:07:35 -0500

    Add comments for readablilty

--- dbscan/dbscan.py
+++ dbscan/dbscan.py
@@ -1,7 +1,6 @@
 import numpy
+from typing import List
 
-
-#
 
 class DBSCAN:
     def __init__(self, min_points, epsilon):
@@ -18,21 +17,38 @@ class DBSCAN:
         cluster_id = 0
 
         for point in range(0, len(data)):
+            # This loop will ensure we get all the clusters
 
-            if not (labels[point] == 0):
+            if labels[point] != 0:
+                # Point is already labeled
                 continue
 
             neighbor_points = self.region_query(data, point)
 
             if len(neighbor_points) < self.min_points:
+                # Noise or border point. Cannot expand this point.
                 labels[point] = -1
             else:
+                # Point is a core point
                 cluster_id += 1
                 self.grow_cluster(data, labels, point, neighbor_points, cluster_id)
 
         return labels
 
-    def grow_cluster(self, data, labels, point, neighbor_points, cluster_id):
+    def grow_cluster(self, data, labels, point, neighbor_points, cluster_id) -> None:
+        """Grows the current cluster and assigns labels to any other core or border points.
+
+        neighbor_points can be thought of as a FIFO queue. It will continually grow.
+        Once we've expanded all the neighbor points than we know we have maximally
+        expanded the current cluster.
+
+        :param data: List of 2d points.
+        :param labels: List of labels: 1 for each 2d point in data.
+        :param point: Current point in the data set.
+        :param neighbor_points: Neighbor points of point.
+        :param cluster_id: The current cluster id.
+        :return: None
+        """
         labels[point] = cluster_id
 
         i = 0
@@ -41,17 +57,28 @@ class DBSCAN:
             neighbor_point = neighbor_points[i]
 
             if labels[neighbor_point] == -1:
+                # Relabel the noise point as belonging to this cluster (i.e., border point)
                 labels[neighbor_point] = cluster_id
+
             elif labels[neighbor_point] == 0:
                 labels[neighbor_point] = cluster_id
 
                 neighbor_point_neighborhood = self.region_query(data, neighbor_point)
 
                 if len(neighbor_point_neighborhood) >= self.min_points:
-                    neighbor_points = neighbor_points + neighbor_point_neighborhood
+                    # This neighborhood has more than min_points,
+                    # So add it the neighbor_points FIFO queue
+                    neighbor_points += neighbor_point_neighborhood
+
             i += 1
 
-    def region_query(self, data, this_point):
+    def region_query(self, data, this_point) -> List[List]:
+        """Queries for list of 2d points that are in the epsilon neighborhood of this_point.
+
+        :param data: List of 2d points.
+        :param this_point: Point who's region will be queried
+        :return: List of 2d points that are in the epsilon neighborhood of this_point.
+        """
         neighbors = []
 
         for point in range(0, len(data)):
